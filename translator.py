a95e031f085f456cc28e502e6188f34ac3b0a04f
Chen <2401256141@qq.com>
2025-06-24 17:37:26 +0800

    ignore: 忽略更新

--- translator.py
+++ translator.py
@@ -8,6 +8,7 @@ class BergamotTranslator:
     def translate(cls, text):
         response = cls.client.post(
             url="http://localhost:8080/translate",
+            # url="http://192.168.2.20:8080/translate",
             json={
                 "text": text,
             },
